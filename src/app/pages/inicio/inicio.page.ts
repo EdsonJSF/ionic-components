f6a285dde33ee920fecbd75662fe84e9ff0d8920
EdsonJSF <edsonjsf1990@gmail.com>
2022-01-05 09:57:20 -0400

    ion list para ras rutas en el inicio

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+  components: Componente[] = [
+    {
+      icon: 'albums-outline',
+      name: 'action sheet',
+      redirectTo: '/action-sheet',
+    },
+    {
+      icon: 'alert-circle-outline',
+      name: 'alert',
+      redirectTo: '/alert',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
